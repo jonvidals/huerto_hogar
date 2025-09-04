badca56b19654ae365a0669f7af5bdc4578bc4ff
jonvidals <jon.vidals@duocuc.cl>
2025-09-04 00:07:13 -0400

    comit 981249898124

--- static/script.js
+++ static/script.js
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+const hero = document.querySelector('.hero');
+const images = [
+  'https://images.pexels.com/photos/574919/pexels-photo-574919.jpeg',
+  'https://images.pexels.com/photos/19162212/pexels-photo-19162212/free-photo-of-agricultura-granja-manzanas-fruta.jpeg',
+  'https://images.pexels.com/photos/18510514/pexels-photo-18510514/free-photo-of-comida-sano-naturaleza-rojo.jpeg'
+];
+let index = 0;
+function changeHero() {
+    index = (index + 1) % images.length;
+    hero.style.backgroundImage = `url('${images[index]}')`;
+}
+hero.style.backgroundImage = `url('${images[0]}')`;
+setInterval(changeHero,5000);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const body = document.body;
+    menuToggle.addEventListener('click', () => body.classList.toggle('menu-open'));
+    window.addEventListener("scroll", () => window.scrollY > 50 ? body.classList.add("scrolled") : body.classList.remove("scrolled"));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.querySelector(".btn-search-toggle");
+    const searchFormDesktop = document.querySelector(".search-form");
+    toggleBtn.addEventListener("click", () => {
+        if(window.innerWidth > 600){
+            searchFormDesktop.classList.toggle("active");
+            if(searchFormDesktop.classList.contains("active")) searchFormDesktop.querySelector(".search-box").focus();
+        }
+    });
+});
+=======
 // ==================== HERO SLIDER ====================
 const hero = document.querySelector('.hero');
 
@@ -54,3 +87,4 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+>>>>>>> d329d4376cb592ba4c96fd9ac80667f4661885f3
